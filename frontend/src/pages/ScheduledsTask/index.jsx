765f06b3d1e29bb2adfc412816a50bf06a58ac56
Samuellucas97 <samuellucas97@ufrn.edu.br>
2020-12-08 07:42:03 -0300

    at

--- frontend/src/pages/ScheduledsTask/index.jsx
+++ frontend/src/pages/ScheduledsTask/index.jsx
@@ -1,129 +1,32 @@
+import { useState, useEffect } from 'react';
 
-import DefaultLayout from '../../layouts/DefaultLayout'
+import { useHistory } from 'react-router-dom';
 import { Grid, CardCustom } from '../../components/index'
+import DefaultLayout from '../../layouts/DefaultLayout'
 import { TitleStyled } from './style'
 
-const tasks = [
-    {
-      id: 0,
-      title: 'Featured post',
-      finishDate: 'Nov 12',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 1,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 2,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        '',
-    },
-    {
-      id: 3,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 4,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 5,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 6,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 7,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 8,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 9,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 10,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 11,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 12,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 13,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 14,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-    {
-      id: 15,
-      title: 'Post title',
-      finishDate: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    },
-  ];
+import api from '../../services/api'
 
 export default function ScheduledsTasks() {
+
+  const history = useHistory();
+  const [tasksScheduler, setTasksScheduler] = useState([]);
+
+  const idUser = localStorage.getItem('idUser')
+  
+  useEffect(() => { 
+      api.get(`tasks/scheduler/${idUser}`)
+      .then(response => {
+          setTasksScheduler(response.data);
+      })
+  }, [idUser]);
+
+
     return ( 
         <DefaultLayout> 
             <TitleStyled>Tarefas agendadas</TitleStyled>
             <Grid container  style={{justifyContent: 'space-around'}} >
-                {tasks.map(task =>(
+                {tasksScheduler.map(task =>(
                   <CardCustom key={task.id}  task={task} />                
                 ))}
             </Grid>
